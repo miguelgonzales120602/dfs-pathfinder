8237440b2d5adcf3bc320d33c8747d1513888647
miguelgonzales120602 <127128233+miguelgonzales120602@users.noreply.github.com>
2023-06-27 19:30:14 +0800

    Update dfs_pathfinder.py
    Updated color palette

--- source/pathfinder/dfs_pathfinder.py
+++ source/pathfinder/dfs_pathfinder.py
@@ -5,13 +5,11 @@ WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("DFS Pathfinder")
 
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-PURPLE = (128, 0, 128)
+CLOSED_COLOR = (66, 70, 140)
+OPEN_COLOR = (245, 167, 200)
+BG_COLOR = (240, 241, 249)
+BARRIER_COLOR = (3, 10, 140)
+PATH_COLOR = (92, 116, 245)
 ORANGE= (255, 165, 0)
 GREY = (128, 128, 128)
 TURQUOISE = (64, 224, 208)
@@ -22,7 +20,7 @@ class Node:
                 self.col = col
                 self.x = row * width
                 self.y = col * width
-                self.color = WHITE
+                self.color = BG_COLOR
                 self.neighbors = []
                 self.width = width
                 self.total_rows = total_rows
@@ -31,13 +29,13 @@ class Node:
                 return self.row, self.col
 
         def is_closed(self):
-                return self.color == RED
+                return self.color == CLOSED_COLOR
 
         def is_open(self):
-                return self.color == GREEN
+                return self.color == OPEN_COLOR
 
         def is_barrier(self):
-                return self.color == BLACK
+                return self.color == BARRIER_COLOR
 
         def is_start(self):
                 return self.color == ORANGE
@@ -46,25 +44,25 @@ class Node:
                 return self.color == TURQUOISE
 
         def reset(self):
-                self.color = WHITE
+                self.color = BG_COLOR
 
         def make_start(self):
                 self.color = ORANGE
 
         def make_closed(self):
-                self.color = RED
+                self.color = CLOSED_COLOR
 
         def make_open(self):
-                self.color = GREEN
+                self.color = OPEN_COLOR
 
         def make_barrier(self):
-                self.color = BLACK
+                self.color = BARRIER_COLOR
 
         def make_end(self):
                 self.color = TURQUOISE
 
         def make_path(self):
-                self.color = PURPLE
+                self.color = PATH_COLOR
 
         def draw(self, win):
                 pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
@@ -143,7 +141,7 @@ def draw_grid(win, rows, width):
                         pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
 
 def draw(win, grid, rows, width):
-        win.fill(WHITE)
+        win.fill(BG_COLOR)
 
         for row in grid:
                 for node in row:
